27862f95db46d9b93713740e9620cdb256c16b75
Zoumi <61654296+Zoumi-Dev@users.noreply.github.com>
2020-05-24 02:03:13 +0200

    ajout d'un message lorsque Merlin rejoint un server

--- Merlin/commands/infos/server-infos.js
+++ Merlin/commands/infos/server-infos.js
@@ -12,6 +12,7 @@ module.exports.run = async (client, message, args) => {
         .addField("> :crown: | Propriétaire", `• <@${message.guild.owner.id}>`, true)
         .addField("> :mag: | Serveur créer le", `• ${moment.utc(message.guild.createdAt).format("DD/MM/YYYY à HH:mm")}`, true)
         .addField("> :busts_in_silhouette: | Membre(s) total", `• ${message.guild.memberCount}`, true)
+        .addField("> Robot(s)", `• `)
         .addField("> :closed_book: | Channel(s)", `• ${message.guild.channels.cache.size}`, true)
         .setTimestamp()
         .setFooter("Merlin | Powered by Zoumi#0336");
---
+++ Merlin/events/guild/guildCreate.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+
+module.exports = async (client, guild) => {
+
+    if (client.guilds.cache.get('712358618993000499').channels.cache.find(ch => ch.name === client.config.DEFAULT_SETTINGS.logsChannel)) {
+        let joinEmbed = new Discord.MessageEmbed()
+            .setAuthor("Merlin")
+            .setColor("GREY")
+            .setDescription(`j'ai été ajouter sur le server \`${guild.name}\` !`)
+            .setTimestamp()
+            .setFooter("Merlin | Powered by Zoumi#0336");
+        return client.guilds.cache.get("712358618993000499").channels.cache.find(ch => ch.id === "713886808798396417").send(joinEmbed);
+    }
+};
