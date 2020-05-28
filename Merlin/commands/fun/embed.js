8de58362f893740c6527c8a119e13a34b5730f92
Zoumi-Dev <venosia.pro@gmail.com>
2020-05-29 01:18:57 +0200

    Mise à jour

---
+++ Merlin/commands/fun/embed.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+
+module.exports.run = (client, message, args) => {
+
+    message.delete();
+
+    let sayEmbed = args.join(' ');
+
+    if (!sayEmbed){
+        let embed = new Discord.MessageEmbed()
+            .setAuthor("Merlin")
+            .setColor("GREEN")
+            .addField("> :x: | Erreur", "`Utilisation: _embed [message]`")
+            .setTimestamp()
+            .setFooter(client.config.footer);
+        return message.channel.send(embed);
+    }
+
+    let embed = new Discord.MessageEmbed()
+        .setAuthor("Merlin")
+        .setDescription(`${sayEmbed}`)
+        .setColor("GREEN")
+        .setTimestamp()
+        .setFooter(client.config.footer);
+    return message.channel.send(embed);
+
+};
+
+module.exports.help = {
+    name: "embed",
+    description: "Permet d'envoyer un embed.",
+    usage: "_embed [message]",
+};
