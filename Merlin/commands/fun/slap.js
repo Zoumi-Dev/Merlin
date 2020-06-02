5c9fa993b97030faae9de58123033628882270cf
Zoumi-Dev <venosia.pro@gmail.com>
2020-06-02 14:36:47 +0200

    ajout _invite et _slap

---
+++ Merlin/commands/fun/slap.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+const fetch = require('node-fetch');
+
+module.exports.run = async (client, message, args) => {
+
+    message.delete();
+
+    let usr = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));
+
+    var gif = await fetch("https://nekos.life/api/v2/img/slap")
+        .then(res => res.json())
+        .then(json => json.url);
+
+    if (!args[0]) {
+        let embed = new Discord.MessageEmbed()
+            .setAuthor("Merlin")
+            .setColor("GREEN")
+            .setDescription(`> <@${message.author.id}> a foutue une tarte à <@${client.user.id}>`)
+            .setImage(gif)
+            .setTimestamp()
+            .setFooter(client.config.footer);
+        return message.channel.send(embed);
+    }else{
+        if (args[0] === usr){
+            let embed = new Discord.MessageEmbed()
+                .setAuthor("Merlin")
+                .setColor("GREEN")
+                .setDescription(`> <@${message.author.id}> a foutue une tarte à <@${usr.user.id}>`)
+                .setImage(gif)
+                .setTimestamp()
+                .setFooter(client.config.footer);
+            return message.channel.send(embed);
+        }
+    }
+};
+
+module.exports.help = {
+    name: "slap",
+};
--- Merlin/commands/infos/invite.js
+++ Merlin/commands/infos/invite.js
@@ -7,7 +7,7 @@ module.exports.run = async (client, message, args) => {
     let embed = new Discord.MessageEmbed()
         .setAuthor("Merlin")
         .setColor("BLUE")
-        .setDescription(`> Hey <@${message.author.id}>, click [[ici]](https://discord.gg/7PQUHTr) si tu souhaites inviter le bot sur ton serveur discord ou click [[ici]](https://discord.com/api/oauth2/authorize?client_id=712318774644310057&permissions=8&scope=bot) si tu souhaites rejoindre le serveur help de __**Merlin**__.`)
+        .setDescription(`> Hey <@${message.author.id}>, click [[ici]](https://discord.gg/7PQUHTr) si tu souhaite inviter le bot sur ton serveur discord ou click [[ici]](https://discord.com/api/oauth2/authorize?client_id=712318774644310057&permissions=8&scope=bot) si tu souhaite rejoindre le serveur help de __**Merlin**__.`)
         .setTimestamp()
         .setFooter(client.config.footer);
     return message.channel.send(embed);
@@ -15,5 +15,5 @@ module.exports.run = async (client, message, args) => {
 };
 
 module.exports.help = {
-  name: "invite",
+    name: "invite",
 };
