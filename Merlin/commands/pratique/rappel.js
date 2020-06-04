d82b6de11c9c401f1b87ce977408d25904b5f7b9
Zoumi-Dev <venosia.pro@gmail.com>
2020-06-05 00:29:16 +0200

    ajout de la commande _rappel

---
+++ Merlin/commands/pratique/rappel.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+const ms = require('ms');
+
+module.exports.run = async (client, message, args) => {
+
+    message.delete();
+
+    let rappel = args.slice(1).join(" ");
+
+    if (args < 1){
+        let noargs = new Discord.MessageEmbed()
+            .setAuthor("Merlin")
+            .setColor("GREY")
+            .addField("> :x: | Erreur", "`Utilisation: _rappel [temps] [message]`")
+            .setTimestamp()
+            .setFooter(client.config.footer);
+        return message.channel.send(noargs);
+    } setTimeout(() => {
+        message.guild.member(message.author.id).send(`${client.emojis.cache.find(e => e.id === client.emo.kawai)} | Rappel: ${rappel}`);
+    }, ms(args[0]));
+
+    let rappelEmbed = new Discord.MessageEmbed()
+        .setAuthor("Merlin")
+        .setColor("GREY")
+        .setDescription(`> <@${message.author.id}>, je vous rappellerez dans \`${args[0]}\` pour la raison \`${rappel}\``)
+        .setTimestamp()
+        .setFooter(client.config.footer);
+    return message.channel.send(rappelEmbed);
+};
+
+module.exports.help = {
+    name: "rappel",
+    aliases: ["rpl"],
+    categories: "pratique",
+};
